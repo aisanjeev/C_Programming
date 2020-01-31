54aeeab5f4ddc1a53bf3e312c49f647cb17288ad
Sanjeev Kumar <59331453+aisanjeev@users.noreply.github.com>
2020-01-31 13:18:51 +0530

    Add files via upload

---
+++ DEMOOO.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m1=50,m2=40;
+clrscr();
+if(m1>=50)
+{
+if(m2>=50)
+{
+printf("Pass");
+}
+else
+{
+printf("Fail");
+}
+}
+else
+{
+printf("Fail");
+}
+getch();
+}
